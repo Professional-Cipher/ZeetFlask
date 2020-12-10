875582c3f4acfadfb6e47ffd84c5e538f4dc415e
Apoorv Garg <u.garg.10@gmail.com>
2020-12-10 15:04:30 +0530

    Commit

--- index.py
+++ index.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>ZeetFlask</h1>"
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=False,host='0.0.0.0')
+    app.run()#(debug=False,host='0.0.0.0')
